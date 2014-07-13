f50d9cc3cc26fb2aea01c9d4ecc0a030c6acea98
kavitshah8 <kavit.shah8@gmail.com>
2014-07-12 22:24:48 -0400

    adds Jebbit logo

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,5 @@
 export default Ember.Controller.extend({
-	url: "http://i.imgur.com/YuDUg7q.jpg",
+	url: "http://c93fea60bb98e121740fc38ff31162a8.s3.amazonaws.com/wp-content/uploads/2013/08/New-Jebbit-Logo-1.png",
 	
 	actions:{
 		checkPassword: function(){
